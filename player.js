8c191b48d679218988994c950c7ff20212714a0b
SuuYii <sue2gakupo@gmail.com>
2025-06-13 00:01:03 +0800

    feat(js): show "Now Playing" after clicking mute button

--- player.js
+++ player.js
@@ -1,19 +1,13 @@
-//開發的想法是：各個功能&畫面依序做
 //功能區
-
-
-//使用者觸發click事件，則播放音樂
 var myMusic = document.getElementById("myMusic");
 var volumeControl = document.getElementById("volumeControl");
-var information = document.getElementById("information")
-var progressBar = document.getElementById("progressBar"); //進度條
+var information = document.getElementById("information");
+var progressBar = document.getElementById("progressBar");
 var musicList = document.getElementById("musicList")
-var functionButtons = document.getElementById("functionButtons"); //功能鈕
+var functionButtons = document.getElementById("functionButtons");
 
-
-var txtVolume = volumeControl.children[3]; //音量顯示的input
-var rangeVolume = volumeControl.children[0]; //音量調整的range
-
+var txtVolume = volumeControl.children[3]; //音量顯示input
+var rangeVolume = volumeControl.children[0]; //音量調整range
 
 var musicDuration = information.children[0];
 var playStatus = information.children[1];
@@ -26,33 +20,28 @@ var infoStatus = information.children[2]; //單曲循環鈕
 function musicStatus() {
 
     if (infoStatus.innerText == "單曲循環") {
-        changeMusic(0); //單曲循環
-    } 
+        changeMusic(0);
+    }
     else if (infoStatus.innerText == "隨機播放") {
-        var n = Math.floor(Math.random() * musicList.length); //隨機在musiclist中選擇一首音樂
-        changeMusic(n - musicList.selectedIndex); //隨機播放音樂
-    } 
+        var n = Math.floor(Math.random() * musicList.length);
+        changeMusic(n - musicList.selectedIndex);
+    }
     else if (infoStatus.innerText == "全曲循環" && musicList.length == musicList.selectedIndex + 1) {
         changeMusic(0 - musicList.selectedIndex);
-    } 
-    else if (musicList.length == musicList.selectedIndex + 1) { //是否為最後一首歌
+    }
+    else if (musicList.length == musicList.selectedIndex + 1) {   //是否為最後一首歌
         stopMusic();
-    } 
-    else {//不是最後一首歌就播下一首歌
+    }
+    else {    //不是最後一首歌就播下一首歌
         changeMusic(1);
     }
 }
 
 
-function loopOne() {
-    infoStatus.innerHTML = infoStatus.innerHTML == "單曲循環" ? "正常" : "單曲循環";
-}
-function setRandom() {
-    infoStatus.innerHTML = infoStatus.innerHTML == "單曲循環" ? "正常" : "隨機播放";
-}
-function loopAll() {
-    infoStatus.innerHTML = infoStatus.innerHTML == "單曲循環" ? "正常" : "全曲循環";
-}//三個按鈕做互斥
+function setMode(mode) {
+    infoStatus.innerHTML = infoStatus.innerHTML == mode ? "正常" : mode;
+} //播放模式做互斥
+
 
 
 function changeMusic(n) {
@@ -102,7 +91,7 @@ function setMusicDuration() {
     var w = myMusic.currentTime / myMusic.duration * 100;
 
     // 更新進度條的背景漸層
-    progressBar.style.backgroundImage = `linear-gradient(to right, rgb(243, 208, 167) ${w}%,rgb(236, 236, 234) ${w}%)`;
+    progressBar.style.backgroundImage = `linear-gradient(to right, rgb(245, 173, 92) ${w}%,rgb(236, 236, 234) ${w}%)`;
 
     /////////////////////////////////////////////////////////////
 
@@ -113,7 +102,7 @@ function setMusicDuration() {
 //目前歌曲長度初始化
 function ProgressInitial() {
     progressBar.max = myMusic.duration * 10000; // 設置進度條的最大值為音樂的總時長
-    setInterval(setMusicDuration, 10);// 每秒更新一次進度條
+    setInterval(setMusicDuration, 5);// 每秒更新一次進度條
 }
 
 setVolumeByRangeBar(); //初始化音量
@@ -123,8 +112,7 @@ setVolumeByRangeBar(); //初始化音量
 function setVolumeByRangeBar() {
     //console.log(event.target.value)
     txtVolume.value = rangeVolume.value;
-    myMusic.volume = txtVolume.value / 100; //真正寫入音量屬性值
-    //用漸層-處理音量bar隨著button移動(塗音量條的顏色)
+    myMusic.volume = txtVolume.value / 100;
     rangeVolume.style.backgroundImage = `linear-gradient(to right, rgba(252, 160, 55, 0.8) ${rangeVolume.value}%,rgb(255, 255, 255) ${rangeVolume.value}%)`;
 
 }
@@ -145,25 +133,16 @@ function changeVolume(v) {
 function setMute() {
     myMusic.muted = !myMusic.muted;
     event.target.className = event.target.className == "setMute" ? "" : "setMute";
-    updateInfo(myMusicS.muted ? "已靜音" : "取消靜音");
-    
+    updateInfo(myMusic.muted ? "已靜音" : "取消靜音");
+    setTimeout(() => {
+        updateInfo("目前播放" + myMusic.title);
+    }, 1000);
 }
 
-//快轉倒轉同一個按鈕//有參數的function//同一個參數不同結果
+//快轉倒轉
 function changeTime(s) {
     myMusic.currentTime += s;
 }
-
-/*  //倒轉
-     function prevTime() {
-         myMusic.currentTime -= 5;
-     }
-
-     //快轉//比原本時間多5秒
-     function nextTime() {
-         myMusic.currentTime += 5;
-     } */
-
 
 function updateInfo(text) {
     playStatus.innerHTML = text;
@@ -180,7 +159,6 @@ function playMusic() {
 
     ProgressInitial(); //音樂開始播放時，才開始更新進度條的值
 
-    //playStatus.innerHTML = "目前播放" + myMusic.title;
     updateInfo("目前播放" + myMusic.title);
 }
 
@@ -190,17 +168,14 @@ function pauseMusic() {
     myMusic.pause();
     event.target.innerHTML = "4";
     event.target.onclick = playMusic;
-    //playStatus.innerHTML = "音樂暫停";
     updateInfo("音樂暫停");
 }
 
 //停止音樂
 function stopMusic() {
-    //音樂停下，且 進度條歸零
     myMusic.pause();
-    myMusic.currentTime = 0; //點擊停止鈕會跳到設定的時間
-    event.target.previousElementSibling.innerHTML = "4";//抓到上一個兄弟節點
-    event.target.previousElementSibling.onclick = playMusic; //恢復播放音樂的功能
-    //playStatus.innerHTML = "音樂停止";
+    myMusic.currentTime = 0;
     updateInfo("音樂停止");
+    btnPlay.innerText = "4";
+    btnPlay.onclick = playMusic;
 }
